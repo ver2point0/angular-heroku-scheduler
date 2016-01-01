367881d9ca686beb9eb52d6ca8b8524499d8c609
John McIntosh II <jmci10@hotmail.com>
2016-01-01 05:47:44 +0000

    finish updates

--- scheduler.js
+++ scheduler.js
@@ -1,32 +1,27 @@
 var scheduler = angular.module("scheduler",
   ["xeditable", "ngMockE2E", "ngAnimate", "ui.bootstrap"]);
 
-scheduler.run(function(themeOptions) {
-  themeOptions.theme = "bs3";
+scheduler.run(function(editableOptions) {
+  editableOptions.theme = "bs3";
 });
 
 scheduler.controller("Control", function($scope, $filter, $http) {
   
   $scope.tasks = [{
     id: 0, name: "John Doe", dynoSize: "Free", frequency: "Every 1 minute",
-    lastRun: "Never", nextDue: "00:01"
+    lastRun: "Never", nextDue: "00:01",
   }];
   
   $scope.dynoSizes = [ "Free" ];
   
-  $scope.frequencies = [ "Daily", "Hourly", "Every 1 Minute"];
+  $scope.frequencies = [ "Daily", "Hourly", "Every 1 Minute" ];
+  
+  $scope.lastRun = [ "Never", "Yesterday" ];
   
   $scope.times = [ "00:01", "00:02", "00:03" ];
   
   $scope.newDate = new Date();
   $scope.newDate.setDate($scope.newDate.getDate() + 1);
-  
-  $scope.addTask = function() {
-    $scope.inserted = {
-      id: $scope.tasks.length + 1
-    };
-    $scope.tasks.push($scope.inserted);
-  };
   
   $scope.saveTask = function(data, id) {
     angular.extend(data, { id: id } );  
@@ -34,7 +29,14 @@ scheduler.controller("Control", function($scope, $filter, $http) {
   };
   
   $scope.removeTask = function(index) {
-    $scope.tasks.splice(index, 0);
+    $scope.tasks.splice(index, 1);
+  };
+  
+  $scope.addTask = function() {
+    $scope.inserted = {
+      id: $scope.tasks.length + 1,
+    };
+    $scope.tasks.push($scope.inserted);
   };
   
 });
